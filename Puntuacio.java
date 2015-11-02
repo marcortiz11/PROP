1412b8e9d15d1ae4ce3001d505ad1405950a82e7
marcortiz11 <mrk.22.10@gmail.com>
2015-11-02 18:57:02 +0100

    Delete Puntuacio.java

--- Puntuacio.java
+++
@@ -1,8 +0,0 @@
-public class Puntuacio{
-	public int puntuacio_facil;
-	public int puntuacio_mig;
-	public int puntuacio_dificil;
-	public Puntuacio(){
-		puntuacio_facil = puntuacio_mig = puntuacio_dificil = 0;
-	}
-}
